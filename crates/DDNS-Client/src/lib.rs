c036020e530c4c6a47a056b16c4674c2cd7c2a2f
derrick921213 <dlin12457@gmail.com>
2026-03-11 14:47:25 +0800

    Refactor: Change get_public_ip function to public and update logging in main function

--- crates/DDNS-Client/src/lib.rs
+++ crates/DDNS-Client/src/lib.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
 use std::net::{IpAddr, Ipv4Addr};
 #[allow(unused)]
-async fn get_public_ip() -> Result<Ipv4Addr> {
+pub async fn get_public_ip() -> Result<Ipv4Addr> {
     match public_ip_address::perform_lookup(None).await {
         Ok(resp) => {
             if let IpAddr::V4(ip) = resp.ip {
-                println!("Public IPv4 Address: {}", ip);
                 Ok(ip)
             } else {
                 Err(anyhow::anyhow!("Received an unexpected IP type"))
--- crates/DDNS-Client/src/main.rs
+++ crates/DDNS-Client/src/main.rs
@@ -1,5 +1,7 @@
+use tracing::{debug, info};
 use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 use anyhow::Result;
+use ddns_client::get_public_ip;
 #[tokio::main]
 async fn main() -> Result<()> {
     let stdout_layer = fmt::layer().with_target(true).with_thread_ids(true);
@@ -7,6 +9,7 @@ async fn main() -> Result<()> {
         EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info")).unwrap();
 
     tracing_subscriber::registry().with(filter_layer).with(stdout_layer).init();
-    println!("Hello, world!");
+    info!("Starting DDNS Client...");
+    debug!("Client IP: {}", get_public_ip().await?);
     Ok(())
 }
