ed1221f925e5c7f4ec6b898034ad7d77c2c10c56
derrick921213 <dlin12457@gmail.com>
2026-04-14 16:04:59 +0800

    refactor: remove commented-out Cloudflare client code from main function

--- crates/DDNS-Server/src/main.rs
+++ crates/DDNS-Server/src/main.rs
@@ -93,28 +93,5 @@ async fn main() -> Result<()> {
         parser::repl::run(&ctx).await?;
     }
     
-
-    // let cf_token = "ad74vvoDGK0M3aE5Lzgzy8aZDZsXvaYvHP5p0Hfn";
-    // let zone_name = "duacodie.com";
-    // info!("Starting DDNS Client");
-    // debug!("Using Cloudflare Token: {}", cf_token);
-    // let cf_h = DnsFactory::create(ProviderType::Cloudflare, cf_token);
-    // let zones = cf_h.list_zones(Some(zone_name)).await?;
-    // if zones.is_empty() {
-    //     error!("No zones found");
-    //     return Ok(());
-    // }
-    // let zt: HashMap<String, String> = zones.into_iter().map(|(id, name)| (name, id)).collect();
-    // debug!("Zone map created: {:#?}", zt);
-    // match zt.get(zone_name) {
-    //     Some(zone_id) => {
-    //         debug!("Zone ID for '{}': {}", zone_name, zone_id);
-    //         let records = cf_h.list_records(zone_id, None).await?;
-    //         debug!("DNS Records for '{}': {:#?}", zone_name, records);
-    //         //TODO: fn (zone_id)
-    //     }
-    //     None => error!("Zone '{}' not found.", zone_name),
-    // }
-
     Ok(())
 }
